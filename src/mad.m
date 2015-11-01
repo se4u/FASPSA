aa64e2bd78e63c7108d94d6b62b1826a6664f855
se4u <pushpendre@gmail.com>
2015-11-01 14:30:52 -0500

    Update mad.m (Add documentation)

--- src/mad.m
+++ src/mad.m
@@ -1,3 +1,6 @@
 function mean_absolute_difference = mad(a, b)
 % MAD - Return the mean absolute difference of input arguments.
+% In this function, a and b are vectors, that represent the 
+% parameter estimate and the true parameters. and the returned
+% value is a scalar.
 mean_absolute_difference = mean(abs(a - b));
