279f869c21f2617cb08d5ebd2a571023de75898a
Pushpendre Rastogi <pushpendre@gmail.com>
2015-11-09 02:24:40 -0500

    Minor formatting

--- src/FeedbackAdaptive2SPSA.m
+++ src/FeedbackAdaptive2SPSA.m
@@ -97,7 +97,6 @@ for k=0:max_iterations
     % settings.sum_ck_square_ck_tilda_square = ...
     %     sum_ccs_update.sum_ck_square_ck_tilda_square;
 
-    % TODO: fix this update.
     % Update Hbar
     hbar_scalar_contrib = (delta_tilda_k' * Hbar * delta_k);
     Hk_hat_minus_Phi_hat_scalar = w_k * (h_k - hbar_scalar_contrib);
--- src/adaptivespsa_common_preconditioning.m
+++ src/adaptivespsa_common_preconditioning.m
@@ -1,5 +1,6 @@
 function Hbarbar = adaptivespsa_common_preconditioning(Hbar, k)
 delta_eye = 1e-8 * exp(-k) * eye(size(Hbar));
 Hbarbar = sqrtm(Hbar * Hbar + delta_eye);
+% Hbarbar = Hbar + delta_eye; % This conditioning is incredibly bad!
 fprintf(2, '\n rcond(Hbar) %f rcond(Hbarbar) %f', ...
         rcond(Hbar), rcond(Hbarbar));
