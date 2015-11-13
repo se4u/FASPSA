44826deedc62e0001cef94f882129dd163eb83fe
Pushpendre Rastogi <pushpendre@gmail.com>
2015-11-13 14:08:14 -0500

    Bring back the old conditioning.

--- src/adaptivespsa_common_preconditioning.m
+++ src/adaptivespsa_common_preconditioning.m
@@ -1,7 +1,11 @@
-function Hbar = adaptivespsa_common_preconditioning(Hbar, k)
-% delta_eye = 1e-8 * exp(-k) * eye(size(Hbar));
-sa = eigs((Hbar+ Hbar')/2, 1, 'SA');
-if sa < 0 
-    regularizer = -sa + 1e-8;
-    Hbar = Hbar + regularizer * eye(size(Hbar));
+function Hbarbar = adaptivespsa_common_preconditioning(Hbar, k)
+delta_eye = 1e-8 * exp(-k);
+Hbar_sq = Hbar * Hbar;
+Hbarbar_f  = @(regularizer) sqrtm(Hbar_sq + regularizer * eye(size(Hbar)));
+Hbarbar = Hbarbar_f(delta_eye);
+while not(all(isreal(Hbarbar)))
+    % exit(1);
+    delta_eye = 10 * delta_eye;
+    my_fprintf(2, '\n delta_eye %g ', delta_eye);
+    Hbarbar = Hbarbar_f(delta_eye);
 end
