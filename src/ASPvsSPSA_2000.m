fc2024584beb65269f692977fccb0a8ee431008f
Pushpendre Rastogi <pushpendre@gmail.com>
2015-11-30 12:27:30 -0500

    Fix indentation

--- src/ASPvsSPSA_2000.m
+++ src/ASPvsSPSA_2000.m
@@ -67,207 +67,207 @@ theta_0=-.52276*ones(p,1);
 %dummy statement for setting dimension of Hhat (avoids occasional error message)
 Hhat=eye(p);
 for j=1:cases
-%INITIALIZATION OF PARAMETER AND HESSIAN ESTIMATES
-  theta=theta_0;
-  theta2=theta;
-  Hbar=500*eye(p);
-  lossold=0;    %lossold calculation is for use in loss-based blocking step below
-  for i=1:avg
-    lossold=lossold+feval(loss,theta2);
-  end
-%*********2SPSA*********
-%Initial iterations of 1SPSA based on N measurements (prior to 2SPSA
-%iterations). Use of N-avg is to account for avg used in setting lossold
-  for k=1:(N-avg)/(2+avg)
-    a_k=a1/(k+A1)^alpha1;
-    c_k=c1/k^gamma1;
-    delta=2*round(rand(p,1))-1;
-    thetaplus=theta2+c_k*delta;
-    thetaminus=theta2-c_k*delta;
-    yplus=feval(loss,thetaplus);
-    yminus=feval(loss,thetaminus);
-    ghat=(yplus-yminus)./(2*c_k*delta);
-%   theta update
-    theta2lag=theta2;
-    theta2=theta2-a_k*ghat;
-    % Steps below perform "blocking" step with "avg" no. of loss evaluations
-    % This blocking is based on extra loss evaluation(s)
-    lossnew=0;
+    %INITIALIZATION OF PARAMETER AND HESSIAN ESTIMATES
+    theta=theta_0;
+    theta2=theta;
+    Hbar=500*eye(p);
+    lossold=0;    %lossold calculation is for use in loss-based blocking step below
     for i=1:avg
-      lossnew=lossnew+feval(loss,theta2);
+        lossold=lossold+feval(loss,theta2);
     end
-    if lossnew > lossold-avg*toleranceloss; %if avg=0, this statement is always false
-      theta2=theta2lag;
-    else                                    %statements to follow are harmless when avg=0
-      lossold1=lossold;
-      lossold=lossnew;
+    %*********2SPSA*********
+    %Initial iterations of 1SPSA based on N measurements (prior to 2SPSA
+    %iterations). Use of N-avg is to account for avg used in setting lossold
+    for k=1:(N-avg)/(2+avg)
+        a_k=a1/(k+A1)^alpha1;
+        c_k=c1/k^gamma1;
+        delta=2*round(rand(p,1))-1;
+        thetaplus=theta2+c_k*delta;
+        thetaminus=theta2-c_k*delta;
+        yplus=feval(loss,thetaplus);
+        yminus=feval(loss,thetaminus);
+        ghat=(yplus-yminus)./(2*c_k*delta);
+        %   theta update
+        theta2lag=theta2;
+        theta2=theta2-a_k*ghat;
+        % Steps below perform "blocking" step with "avg" no. of loss evaluations
+        % This blocking is based on extra loss evaluation(s)
+        lossnew=0;
+        for i=1:avg
+            lossnew=lossnew+feval(loss,theta2);
+        end
+        if lossnew > lossold-avg*toleranceloss; %if avg=0, this statement is always false
+            theta2=theta2lag;
+        else                                    %statements to follow are harmless when avg=0
+            lossold1=lossold;
+            lossold=lossnew;
+        end
+        % Blocking below is based on magnitude of theta change (no loss evals.)
+        if max(abs(theta2lag-theta2)) > tolerancetheta;
+            theta2=theta2lag;
+            lossold=lossold1;     %only relevant if also using loss-based blocking
+        end
     end
-    % Blocking below is based on magnitude of theta change (no loss evals.)
-    if max(abs(theta2lag-theta2)) > tolerancetheta;
-      theta2=theta2lag;
-      lossold=lossold1;     %only relevant if also using loss-based blocking
+    caseiter=j   %print-out of iteration number (for monitoring progress)
+                 %
+                 % START 2SPSA ITERATIONS FOLLOWING INITIALIZATION
+                 %
+    for k=(N-avg)/(2+avg)+1:(N-avg)/(2+avg)+(n-N)/(gH_avg*4+avg)
+        a_k=a2/(k+A2)^alpha2;
+        c_k=c2/k^gamma2;
+        ctilda_k=ctilda/k^gamma2;
+        ghatinput=0;
+        Hhatinput=0;
+        % GENERATION OF AVERAGED GRADIENT AND HESSIAN (NO AVERAGING IF gH_avg=1)
+        for m=1:gH_avg
+            delta=2*round(rand(p,1))-1;
+            thetaplus=theta2+c_k*delta;
+            thetaminus=theta2-c_k*delta;
+            yplus=feval(loss,thetaplus);
+            yminus=feval(loss,thetaminus);
+            ghat=(yplus-yminus)./(2*c_k*delta);
+            % GENERATE THE HESSIAN UPDATE
+            deltatilda=2*round(rand(p,1))-1;
+            thetaplustilda=thetaplus+ctilda_k*deltatilda;
+            thetaminustilda=thetaminus+ctilda_k*deltatilda;
+            % LOSS FUNCTION CALLS
+            yplustilda=feval(loss,thetaplustilda);
+            yminustilda=feval(loss,thetaminustilda);
+            ghatplus=(yplustilda-yplus)./(ctilda_k*deltatilda);
+            ghatminus=(yminustilda-yminus)./(ctilda_k*deltatilda);
+            % STATEMENT PROVIDING AN AVERAGE OF SP GRAD. APPROXS. PER ITERATION
+            ghatinput=((m-1)/m)*ghatinput+ghat/m;
+            deltaghat=ghatplus-ghatminus;
+            for i=1:p
+                Hhat(:,i)=deltaghat(i)./(2*c_k*delta);
+            end
+            Hhat=.5*(Hhat+Hhat');
+            Hhatinput=((m-1)/m)*Hhatinput+Hhat/m;
+        end
+        Hbar=((k-(N-avg)/(2+avg))/(k-(N-avg)/(2+avg)+1))*Hbar+Hhatinput/(k-(N-avg)/(2+avg)+1);
+        %   THE THETA UPDATE (FORM BELOW USES GAUSSIAN ELIMINATION TO AVOID DIRECT
+        %   COMPUTATION OF HESSIAN INVERSE)
+        Hbarbar=sqrtm(Hbar*Hbar)+.000001*eye(p)/k;
+        theta2lag=theta2;
+        theta2=theta2-a_k*(Hbarbar\ghatinput);
+        %   Steps below perform "blocking" step with "avg" no. of loss evaluations
+        lossnew=0;
+        for i=1:avg
+            lossnew=lossnew+feval(loss,theta2);
+        end
+        if lossnew > lossold-avg*toleranceloss;
+            theta2=theta2lag;
+        else
+            lossold1=lossold;
+            lossold=lossnew;
+        end
+        if max(abs(theta2lag-theta2)) > tolerancetheta;
+            theta2=theta2lag;
+            lossold=lossold1;
+        end
     end
- end
- caseiter=j   %print-out of iteration number (for monitoring progress)
-%
-% START 2SPSA ITERATIONS FOLLOWING INITIALIZATION
-%
-for k=(N-avg)/(2+avg)+1:(N-avg)/(2+avg)+(n-N)/(gH_avg*4+avg)
-    a_k=a2/(k+A2)^alpha2;
-    c_k=c2/k^gamma2;
-    ctilda_k=ctilda/k^gamma2;
-    ghatinput=0;
-    Hhatinput=0;
-% GENERATION OF AVERAGED GRADIENT AND HESSIAN (NO AVERAGING IF gH_avg=1)
-    for m=1:gH_avg
-      delta=2*round(rand(p,1))-1;
-      thetaplus=theta2+c_k*delta;
-      thetaminus=theta2-c_k*delta;
-      yplus=feval(loss,thetaplus);
-      yminus=feval(loss,thetaminus);
-      ghat=(yplus-yminus)./(2*c_k*delta);
-% GENERATE THE HESSIAN UPDATE
-      deltatilda=2*round(rand(p,1))-1;
-      thetaplustilda=thetaplus+ctilda_k*deltatilda;
-      thetaminustilda=thetaminus+ctilda_k*deltatilda;
-% LOSS FUNCTION CALLS
-      yplustilda=feval(loss,thetaplustilda);
-      yminustilda=feval(loss,thetaminustilda);
-      ghatplus=(yplustilda-yplus)./(ctilda_k*deltatilda);
-      ghatminus=(yminustilda-yminus)./(ctilda_k*deltatilda);
-% STATEMENT PROVIDING AN AVERAGE OF SP GRAD. APPROXS. PER ITERATION
-      ghatinput=((m-1)/m)*ghatinput+ghat/m;
-      deltaghat=ghatplus-ghatminus;
-      for i=1:p
-        Hhat(:,i)=deltaghat(i)./(2*c_k*delta);
-      end
-      Hhat=.5*(Hhat+Hhat');
-      Hhatinput=((m-1)/m)*Hhatinput+Hhat/m;
-    end
-    Hbar=((k-(N-avg)/(2+avg))/(k-(N-avg)/(2+avg)+1))*Hbar+Hhatinput/(k-(N-avg)/(2+avg)+1);
-%   THE THETA UPDATE (FORM BELOW USES GAUSSIAN ELIMINATION TO AVOID DIRECT
-%   COMPUTATION OF HESSIAN INVERSE)
-    Hbarbar=sqrtm(Hbar*Hbar)+.000001*eye(p)/k;
-    theta2lag=theta2;
-    theta2=theta2-a_k*(Hbarbar\ghatinput);
-%   Steps below perform "blocking" step with "avg" no. of loss evaluations
-    lossnew=0;
+    theta2;
+    %
+    %********1SPSA*************
+    % The iterations below are the 1SPSA iterations.  Uses the same gain sequences
+    % as the 1SPSA loop above (where 2SPSA is not fully engaged).  Uses same number
+    % of loss function measurements.  The overall loop is broken into two parts to
+    % accomodate a sliding window of the last IA iterates for an iterate averaging
+    % solution.
+    %
+    IA=200;        %no. of sliding window iterations in iterate averaging
+    lossold=0;     %lossold calculation is for use in loss-based blocking step below
     for i=1:avg
-      lossnew=lossnew+feval(loss,theta2);
+        lossold=lossold+feval(loss,theta);
     end
-    if lossnew > lossold-avg*toleranceloss;
-      theta2=theta2lag;
-    else
-      lossold1=lossold;
-      lossold=lossnew;
+    for k=1:(n-avg)/(2+avg)-IA
+        a_k=a1/(k+A1)^alpha1;
+        c_k=c1/k^gamma1;
+        delta=2*round(rand(p,1))-1;
+        thetaplus=theta+c_k*delta;
+        thetaminus=theta-c_k*delta;
+        yplus=feval(loss,thetaplus);
+        yminus=feval(loss,thetaminus);
+        ghat=(yplus-yminus)./(2*c_k*delta);
+        %   theta update
+        thetalag=theta;
+        theta=theta-a_k*ghat;
+        %   Steps below perform "blocking" steps as in 1SPSA part of 2SPSA above
+        lossnew=0;
+        for i=1:avg
+            lossnew=lossnew+feval(loss,theta);
+        end
+        if lossnew > lossold-avg*toleranceloss; %statement always false if avg=0
+            theta=thetalag;
+        else
+            lossold1=lossold;
+            lossold=lossnew;
+        end
+        if max(abs(thetalag-theta)) > tolerancetheta;
+            theta=thetalag;
+            lossold=lossold1;
+        end
     end
-    if max(abs(theta2lag-theta2)) > tolerancetheta;
-      theta2=theta2lag;
-      lossold=lossold1;
-   end
- end
-theta2;
-%
-%********1SPSA*************
-% The iterations below are the 1SPSA iterations.  Uses the same gain sequences
-% as the 1SPSA loop above (where 2SPSA is not fully engaged).  Uses same number
-% of loss function measurements.  The overall loop is broken into two parts to
-% accomodate a sliding window of the last IA iterates for an iterate averaging
-% solution.
-%
- IA=200;        %no. of sliding window iterations in iterate averaging
- lossold=0;     %lossold calculation is for use in loss-based blocking step below
- for i=1:avg
-   lossold=lossold+feval(loss,theta);
- end
- for k=1:(n-avg)/(2+avg)-IA
-    a_k=a1/(k+A1)^alpha1;
-    c_k=c1/k^gamma1;
-    delta=2*round(rand(p,1))-1;
-    thetaplus=theta+c_k*delta;
-    thetaminus=theta-c_k*delta;
-    yplus=feval(loss,thetaplus);
-    yminus=feval(loss,thetaminus);
-    ghat=(yplus-yminus)./(2*c_k*delta);
-%   theta update
-    thetalag=theta;
-    theta=theta-a_k*ghat;
-%   Steps below perform "blocking" steps as in 1SPSA part of 2SPSA above
-    lossnew=0;
-    for i=1:avg
-      lossnew=lossnew+feval(loss,theta);
+    thetabar=0;
+    for k=(n-avg)/(2+avg)+1-IA:(n-avg)/(2+avg)
+        a_k=a1/(k+A1)^alpha1;
+        c_k=c1/k^gamma1;
+        delta=2*round(rand(p,1))-1;
+        thetaplus=theta+c_k*delta;
+        thetaminus=theta-c_k*delta;
+        %   THE NEXT TWO LINES SHOULD BE CHANGED AS LOSS CHANGES
+        yplus=feval(loss,thetaplus);
+        yminus=feval(loss,thetaminus);
+        ghat=(yplus-yminus)./(2*c_k*delta);
+        %   theta update
+        thetalag=theta;
+        theta=theta-a_k*ghat;
+        %   Steps below perform "blocking" steps as in 1SPSA part of 2SPSA above
+        lossnew=0;
+        for i=1:avg
+            lossnew=lossnew+feval(loss,theta);
+        end
+        if lossnew > lossold-avg*toleranceloss;
+            theta=thetalag;
+        else
+            lossold1=lossold;
+            lossold=lossnew;
+        end
+        if max(abs(thetalag-theta)) > tolerancetheta;
+            theta=thetalag;
+            lossold=lossold1;
+        end
+        thetabar=thetabar+theta;
     end
-    if lossnew > lossold-avg*toleranceloss; %statement always false if avg=0
-      theta=thetalag;
-    else
-      lossold1=lossold;
-      lossold=lossnew;
-    end
-    if max(abs(thetalag-theta)) > tolerancetheta;
-      theta=thetalag;
-      lossold=lossold1;
-   end
-  end
-  thetabar=0;
-  for k=(n-avg)/(2+avg)+1-IA:(n-avg)/(2+avg)
-    a_k=a1/(k+A1)^alpha1;
-    c_k=c1/k^gamma1;
-    delta=2*round(rand(p,1))-1;
-    thetaplus=theta+c_k*delta;
-    thetaminus=theta-c_k*delta;
-%   THE NEXT TWO LINES SHOULD BE CHANGED AS LOSS CHANGES
-    yplus=feval(loss,thetaplus);
-    yminus=feval(loss,thetaminus);
-    ghat=(yplus-yminus)./(2*c_k*delta);
-%   theta update
-    thetalag=theta;
-    theta=theta-a_k*ghat;
-%   Steps below perform "blocking" steps as in 1SPSA part of 2SPSA above
-    lossnew=0;
-    for i=1:avg
-      lossnew=lossnew+feval(loss,theta);
-    end
-    if lossnew > lossold-avg*toleranceloss;
-      theta=thetalag;
-    else
-      lossold1=lossold;
-      lossold=lossnew;
-    end
-    if max(abs(thetalag-theta)) > tolerancetheta;
-      theta=thetalag;
-      lossold=lossold1;
-    end
-    thetabar=thetabar+theta;
-  end
-  %theta
-  thetabar=thetabar/IA;
-  meanHbar=meanHbar+Hbar;
-  errtheta=errtheta+(theta-truetheta)'*(theta-truetheta);
-  errthetaIA=errthetaIA+(thetabar-truetheta)'*(thetabar-truetheta);
-  errtheta2=errtheta2+(theta2-truetheta)'*(theta2-truetheta);
-  lossthetasq=lossthetasq+feval(lossfinaleval,theta)^2;
-  lossthetaIAsq=lossthetaIAsq+feval(lossfinaleval,thetabar)^2;
-  losstheta2sq=losstheta2sq+feval(lossfinaleval,theta2)^2;
-  losstheta=losstheta+feval(lossfinaleval,theta);
-  lossthetaIA=lossthetaIA+feval(lossfinaleval,thetabar);
-  losstheta2=losstheta2+feval(lossfinaleval,theta2);
+    %theta
+    thetabar=thetabar/IA;
+    meanHbar=meanHbar+Hbar;
+    errtheta=errtheta+(theta-truetheta)'*(theta-truetheta);
+    errthetaIA=errthetaIA+(thetabar-truetheta)'*(thetabar-truetheta);
+    errtheta2=errtheta2+(theta2-truetheta)'*(theta2-truetheta);
+    lossthetasq=lossthetasq+feval(lossfinaleval,theta)^2;
+    lossthetaIAsq=lossthetaIAsq+feval(lossfinaleval,thetabar)^2;
+    losstheta2sq=losstheta2sq+feval(lossfinaleval,theta2)^2;
+    losstheta=losstheta+feval(lossfinaleval,theta);
+    lossthetaIA=lossthetaIA+feval(lossfinaleval,thetabar);
+    losstheta2=losstheta2+feval(lossfinaleval,theta2);
 end
 'meanHar/cases'; meanHbar/cases;
 % normalized results of 1SPSA and 2SPSA
 if norm(theta_0-truetheta)~= 0
-  disp('norm(theta_0-truetheta)~= 0; compute normalized results of 1SPSA and 2SPSA');
-  ((errtheta/cases)^.5)/norm(theta_0-truetheta)
-  ((errthetaIA/cases)^.5)/norm(theta_0-truetheta)
-  ((errtheta2/cases)^.5)/norm(theta_0-truetheta)
+    disp('norm(theta_0-truetheta)~= 0; compute normalized results of 1SPSA and 2SPSA');
+    ((errtheta/cases)^.5)/norm(theta_0-truetheta)
+    ((errthetaIA/cases)^.5)/norm(theta_0-truetheta)
+    ((errtheta2/cases)^.5)/norm(theta_0-truetheta)
 end
 % standard dev. of mean of normalized loss values; these are by multiplied by
 % (cases/(cases-1))^.5 to account for loss of degree of freedom in standard
 % deviation calculation before using with t-test
 if cases > 1
     disp('cases > 1');
-  (cases^(-.5))*((cases/(cases-1))^.5)*(lossthetasq/(cases*feval(lossfinaleval,theta_0)^2)-(losstheta/(cases*feval(lossfinaleval,theta_0)))^2)^.5
-  (cases^(-.5))*((cases/(cases-1))^.5)*(lossthetaIAsq/(cases*feval(lossfinaleval,theta_0)^2)-(lossthetaIA/(cases*feval(lossfinaleval,theta_0)))^2)^.5
-  (cases^(-.5))*((cases/(cases-1))^.5)*(losstheta2sq/(cases*feval(lossfinaleval,theta_0)^2)-(losstheta2/(cases*feval(lossfinaleval,theta_0)))^2)^.5
+    (cases^(-.5))*((cases/(cases-1))^.5)*(lossthetasq/(cases*feval(lossfinaleval,theta_0)^2)-(losstheta/(cases*feval(lossfinaleval,theta_0)))^2)^.5
+    (cases^(-.5))*((cases/(cases-1))^.5)*(lossthetaIAsq/(cases*feval(lossfinaleval,theta_0)^2)-(lossthetaIA/(cases*feval(lossfinaleval,theta_0)))^2)^.5
+    (cases^(-.5))*((cases/(cases-1))^.5)*(losstheta2sq/(cases*feval(lossfinaleval,theta_0)^2)-(losstheta2/(cases*feval(lossfinaleval,theta_0)))^2)^.5
 end
 disp('normalized loss values');
 losstheta/(cases*feval(lossfinaleval,theta_0))
