19382bab1aa67bcfb63179f6d2b39d3a78642cf5
Pushpendre Rastogi <pushpendre@gmail.com>
2015-12-15 16:57:13 -0500

    Make the preconditining more bullet proof.

--- src/adaptivespsa_common_preconditioning.m
+++ src/adaptivespsa_common_preconditioning.m
@@ -1,12 +1,16 @@
 function Hbarbar = adaptivespsa_common_preconditioning(Hbar, k)
-delta_eye = 1e-8 * exp(-k);
+Hbar = (Hbar + Hbar')/2;
+delta_eye = max([1e-6 * exp(-k/2) 1e-10]);
+m = max(max(Hbar));
+Hbar = Hbar / m;
 Hbar_sq = Hbar * Hbar;
-Hbarbar_f  = @(regularizer) sqrtm(Hbar_sq + regularizer * eye(size(Hbar)));
-Hbarbar = Hbarbar_f(delta_eye);
+Hbarbar = m * sqrtm(Hbar_sq + delta_eye * eye(size(Hbar)));
 if ~(all(isreal(Hbarbar)))
     disp(eig(Hbar));
     disp(delta_eye);
+    disp(eig(Hbar_sq));
     disp(eig(Hbarbar));
+    pause;
     exit(1);
 end
 % while not(all(isreal(Hbarbar)))
